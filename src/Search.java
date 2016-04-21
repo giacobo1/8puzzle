2a5b2db6677889dfecbe91be70199c862d22258d
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-04-21 15:54:41 -0300

    Implementação parcial da classe Search. Alguns metodos serão transferidos para a classe board, visando facilitar a implementação dos metodos de busca especificos.
    Proximas mudanças:
    
    * Classe Board;
    * Classe Puzzle;
    
    Current Changes:
    	renamed:    board.java -> Board.java
    	new file:   Search.java

---
+++ src/Search.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+
+/* class Search;
+ *
+ * This class represents a framework for its subclasses. It defines the search method as well as
+ * stablishes the common utilities to be used by its subclasses.
+ *
+ * */
+
+public class Search {
+
+	private int nodeCounter;
+	private Board initialBoard;
+	private Board solutionBoard;
+	private ArrayList<Board> solutionPath;
+
+	// It needs two boards for comparision sake.
+	public Search(Board b, Board s) {
+	
+		this.nodeCounter   = 0;
+		this.initialBoard  = b;
+		this.solutionBoard = s;
+		this.solutionPath  = new ArrayList<Board>();
+	}
+
+	// service for graphic interface;
+	public int getTotalOfNodes() {
+		return nodeCounter;
+	}
+
+	// service for graphic interface;
+	public int getSolutionCount() {
+		return this.solutionPath.size();
+	}
+
+	// Add the new-born board to the solutionpath.	
+	public void addToSolutionPath(Board b) {
+		this.solutionPath.add(b);
+	}
+	
+	// service for graphic interface;
+	public ArrayList<Board> getSolutionPath() {
+		return this.solutionPath;
+	}
+
+	// This method implements the actual search. Therefore, it must be overriden by its subclasses;
+	public void run() {}
+
+}
