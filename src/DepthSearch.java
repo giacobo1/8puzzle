df2586aead4a220bc732bbb8daca85fca2e5e218
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-04-24 00:30:50 -0300

    Correção do algoritmo DFS
    * Adicionado arraylist para remover nos repetidos, porem ainda nao
    * implementado;
    
    * Corrigido chamada recursiva do algoritmo DFS;
    * Corrigido calculo de nível da arvore gerada;
    
    TODO:
    
    - Limpara o codigo
    - Otimizar o DFS
    - Testar para entradas maiores (demorando muito)
    
    Changes:
    	modified:   DepthSearch.java

--- src/DepthSearch.java
+++ src/DepthSearch.java
@@ -1,49 +1,80 @@
 import java.util.Stack;
 import java.util.Vector;
+import java.util.ArrayList;
 
 public class DepthSearch extends Search {
 	
 	private int level;
+	private ArrayList<Board> visited;
 
 	DepthSearch(Board b, Board s) {
 		super(b,s);
+
+		this.visited = new ArrayList<Board>();
 
 		this.level = 0;
 	}
 
 
 	// lembrar de contar o numero de nos gerados
-	public void DFS(Board v, Stack<Board> q) {
+	public void DFS(Board v, int n) {
 
+		System.out.format("Level is %d: \n", this.level );
 		v.printBoard();
 		System.out.format("\n");
-		/*	
+			
 		if (v.isEqual(this.metaBoard.getBoard())) {
 		
-			System.out.format("to aqui");
+			System.out.format("to aqui\n");
 
 			this.solutionBoard = v;
 
 			return;
-		}*/
+		}
 
-		if (this.level < 20 ) {
-			this.level++;
+		if (n < 10) {
+			//this.level++;
+
+			n++;
 
 			for (Board b: v.generateNextBoard(v.findWhiteTile())) {
 				
-
-
-				b.setParentNode(v);
-				q.push(b);
+				//if (!isVisited(b)) {
+					
+					this.nodeCounter++;
+					this.visited.add(b);	
+					b.setParentNode(v);
+					DFS(b, n);
+				//}		
 			}
-		
+					
 			// if v is a solution, return
 
-			Board u = q.pop();	
-			DFS(u, q);
+			//Board u = q.pop();	
+			//DFS(u, q);
 		
-		} else return;		
+		} else {
+		       //this.level = 0;	
+			return;
+		
+		}
+
+		
+		return;
+
+	}
+
+	private boolean isVisited(Board b) {
+		
+		boolean result = true;
+
+		for (Board v: this.visited) {
+			result = result && v.isEqual(b.getBoard());
+		}
+
+		
+
+		return !result;
 	}
 
 	// precisa de testes
@@ -59,15 +90,23 @@ public class DepthSearch extends Search {
 	}
 
 	// precisa de testes
+	@Override
 	public void run() {
-
-		Stack<Board> Q = new Stack<Board>();
 		
-		//Q.add(this.initialBoard.generateNextBoard(this.initialBoard.findWhiteTile()));
-		
+		//Vector<Board> firstLevel = this.initialBoard.generateNextBoard(this.initialBoard.findWhiteTile());
 		// acho que tem que ser pra cada no
-		DFS(this.initialBoard, Q);
+		/*
+		for (Board b: firstLevel) {
 		
+			if (!DFS(b)) break;
+
+			this.level = 0;
+		}*/
+
+		DFS(this.initialBoard, 0);
+	
+		System.out.format("Numero de nos criados: %d\n", this.nodeCounter);
+
 		//buildSolutionPath();
 
 
