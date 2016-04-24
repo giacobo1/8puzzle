41af329bb843f99fc60e6f5626140045d28068da
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-04-24 02:01:44 -0300

    Algoritmo DFS funcional
    * Corrigida profundidade do algoritmo
    * Testado com entradas maiores
    * Removidos nós duplicados
    * Metodo buildPath() testado e funcional
    * Nó da solução adicionado corretamento ao solutionpath;
    * Computado numero de nos na solução;
    
    TODO:
    
    - Limpar o codigo
    
    Changes:
    	modified:   DepthSearch.java

--- src/DepthSearch.java
+++ src/DepthSearch.java
@@ -19,35 +19,38 @@ public class DepthSearch extends Search {
 	// lembrar de contar o numero de nos gerados
 	public void DFS(Board v, int n) {
 
+		/*
 		System.out.format("Level is %d: \n", this.level );
 		v.printBoard();
 		System.out.format("\n");
-			
+		*/	
+
 		if (v.isEqual(this.metaBoard.getBoard())) {
 		
-			System.out.format("to aqui\n");
+		//	System.out.format("to aqui\n");
 
 			this.solutionBoard = v;
+
+			this.solutionPath.add(v);
 
 			return;
 		}
 
-		if (n < 10) {
+		if (n < 40) {
 			//this.level++;
 
 			n++;
 
 			for (Board b: v.generateNextBoard(v.findWhiteTile())) {
 				
-				//if (!isVisited(b)) {
+				if (!isVisited(b)) {
 					
 					this.nodeCounter++;
 					this.visited.add(b);	
 					b.setParentNode(v);
 					DFS(b, n);
-				//}		
-			}
-					
+				}
+			}			
 			// if v is a solution, return
 
 			//Board u = q.pop();	
@@ -66,21 +69,22 @@ public class DepthSearch extends Search {
 
 	private boolean isVisited(Board b) {
 		
-		boolean result = true;
+		boolean result = false;
 
 		for (Board v: this.visited) {
-			result = result && v.isEqual(b.getBoard());
+			if (v.isEqual(b.getBoard())) return true;
 		}
 
 		
 
-		return !result;
+		return result;
 	}
 
 	// precisa de testes
 	public void buildSolutionPath() {
 		
 		Board u = solutionBoard.getParentNode();
+
 
 		while( u != null ) {
 			this.solutionPath.add(u);
@@ -104,10 +108,16 @@ public class DepthSearch extends Search {
 		}*/
 
 		DFS(this.initialBoard, 0);
-	
+
 		System.out.format("Numero de nos criados: %d\n", this.nodeCounter);
 
-		//buildSolutionPath();
+		buildSolutionPath();
+
+		System.out.format("Numero de nos na solução: %d\n\n", this.solutionPath.size());
+		for (Board b: this.solutionPath) {
+			b.printBoard();
+			System.out.format("\n");
+		}
 
 
 	}
