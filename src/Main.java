30ae056c9d36163a19c8da27ac7cf1c74d8febee
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2016-04-26 05:20:35 -0300

    Integração da GUI com os Algoritmos
    Implementação parcial dos algoritmos de busca com a GUI. Falta
    sincronizar este branch com o brancho do Michael para acomodar a busca
    em profundidade. Iniciando implementação da animação das jogadas.
    
    Changes:
    	modified:   Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,66 @@ import javafx.scene.paint.Color;
 
 import javafx.geometry.Insets;
 
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.control.RadioButton;
-
+import javafx.beans.value.*;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
+import javafx.application.Application.Parameters;
+
+import java.util.List;
+
+
+import javafx.beans.*;
+
+
+
 
 public class Main extends Application {
-    
+
+	private Board inputBoard;
+	private Board solutionBoard;
+	private Search searchAlgorithm;
+
+	private ArrayList<Board> solutionPath;
+
+	private String search_method_name;
+
+
+	private boolean solvable;
+
+	private Text status;	
+
+	// Aqui vai os algoritmos e inicialização;	
+	@Override
+	public void init() {
+		Parameters p = getParameters();
+		
+		List<String> args = p.getRaw();
+	
+		this.searchAlgorithm = null;
+
+		this.solutionPath = null;
+		this.search_method_name = null;	
+
+		this.inputBoard = new Board(args.get(0));
+		
+		int[][] targetSolution = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+		this.solutionBoard = new Board(targetSolution);
+
+		this.solvable = inputBoard.isSolvable();	
+
+		status = new Text();
+		
+		status.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+
+
+	}
+
+
     @Override
     public void start(Stage primaryStage) {
 
@@ -69,24 +119,74 @@ public class Main extends Application {
 
 	RadioButton breadth = new RadioButton("Breadth-Search");
 	breadth.setToggleGroup(tgroup);
+	breadth.setSelected(true);
+
+	tgroup.selectedToggleProperty().addListener( new ChangeListener<Toggle>() {
+		@Override
+		public void changed(ObservableValue <? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) {
+
+					
+			RadioButton chk = (RadioButton)new_toggle.getToggleGroup().getSelectedToggle();
+			search_method_name = chk.getText(); 
+			    
+			//System.out.format("%s\n", chk.getText());
+		}
+	});
 
 	rbLayout.getChildren().addAll(depthBtn, breadth);
 
 	Button playBtn = new Button("Play");
 	playBtn.setPrefSize(240,20);
+	
+	Button runBtn = new Button("Run");
+	runBtn.setPrefSize(240,20);
+	runBtn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+		@Override
+		public void handle(ActionEvent event) {
+		 
+			if (solvable) {
+				
+				if (search_method_name.equals("Breadth-Search")) {
+					searchAlgorithm = new DepthSearch(inputBoard, solutionBoard); // nao integrei com o codigo do michael ainda [por breadth]	
+				} else {
+					searchAlgorithm = new DepthSearch(inputBoard, solutionBoard); // nao integrei com o codigo do michael ainda [por breadth]	
+				}
+					status.setFill(Color.GREEN);
+					status.setText("SEARCHING ANSWARE ...");
+					
+					searchAlgorithm.run();
+					solutionPath = searchAlgorithm.getSolutionPath();	
+				
+					String totalNodes   = Integer.toString(searchAlgorithm.getTotalOfNodes()); 
+					String solutioNodes = Integer.toString(solutionPath.size()); 
+
+
+					status.setText("Nodos Gerados: " + totalNodes + "\nNodos na Solução: " + solutioNodes ); 
+
+
+			} else {
+				status.setFill(Color.RED);
+				status.setText("UNSOLVABLE PUZZLE");
+			}	
+	    }
+        });
+
+
+
 
 	StackPane summary = new StackPane();
 
 	Rectangle summBackroung = new Rectangle(270,120, Color.BLUE);
 
-	Text status = new Text("Aqui vai o:\n sumario");
+	status = new Text();
 	
 	status.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
 	status.setFill(Color.WHITE);
 
 	summary.getChildren().addAll(summBackroung, status);
 
-	menuLayout.getChildren().addAll(grid, rbLayout, playBtn, summary);
+	menuLayout.getChildren().addAll(grid, rbLayout, runBtn, playBtn, summary);
 
 	
 	layout.getChildren().add(menuLayout);	
@@ -94,7 +194,7 @@ public class Main extends Application {
 
 	layout.setMargin(menuLayout, new Insets(10,45,0,45));
 	
-	Scene scene = new Scene(layout, 320, 480);
+	Scene scene = new Scene(layout, 320, 500);
         primaryStage.setTitle("8puzzle!");
         primaryStage.setScene(scene);
         primaryStage.show();
